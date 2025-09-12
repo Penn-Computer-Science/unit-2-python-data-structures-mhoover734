b24cfa3a530915a1be0c87d5415f29943ffed88e
mhoover734 <mhoover734@phm.k12.in.us>
2025-09-12 09:51:29 -0400

    Update gradebookManager.py

--- gradebookManager.py
+++ gradebookManager.py
@@ -43,24 +43,95 @@ Rubric (40 points total)
 - 10 pts: Correct calculation of averages and top student
 - 10 pts: Clear display of gradebook summary
 '''
-gradebook = {"James": [89, 100, 83], "Jonah": [73, 62, 78], "Jeff": [98, 100, 99]}
-
+gradebook = {"James": [], "Jonah": [73, 62, 78], "Jeff": [98, 100, 99]}
+line_break = "\n"+"-"*30+"\n"
+#[89, 100, 83]
 
 def add_student():
-    name_input = input("\nInput a name to add to the gradebook\nName: ")
-    if not(name_input.lower() in gradebook.lower()):
-        gradebook[name_input] = []
-    else:
-        print("a")
+    print(line_break)
+    while(True):
+        name_input = input("Input a name to add to the gradebook or type \"Cancel\".\nName: ")
+        if name_input.lower() == "cancel":
+            print("\nReturning to Home.")
+            return
+        looped_name = False
+        if name_input == "":
+            print("\nCannot add blank names.\n")
+            looped_name = True
+        for student_name in gradebook:
+            if name_input.lower() == student_name.lower():
+                looped_name = True
+                print(f"\nStudent named \"{name_input}\" already in gradebook.\n")
+                break
+        if not(looped_name):
+            print(f"\nAdded student {name_input} to gradebook.")
+            gradebook[name_input] = []
+            return
 
 def add_grade():
-    print("add grade")
+    print(line_break)
+    while(True):
+        name_query = input("Input a name to add a grade to (Case sensitive) or type \"Cancel\".\nName: ")
+        if name_query.lower() == "cancel":
+            print("\nReturning to Home.")
+            return
+        name_recognized = False
+        for student_name in gradebook:
+            if student_name == name_query:
+                name_recognized = True
+                while True:
+                    try:
+                        grade_input = (input(f"{line_break}\nInput a grade for {name_query} by entering a number between 0-100 or type \"Cancel\".\nGrade: "))
+                        if grade_input.lower() == "cancel":
+                            print("\nReturning to name query.\n"+line_break)
+                            break
+                        if int(grade_input) >= 0 and int(grade_input) <= 100:
+                            gradebook[name_query].append(int(grade_input))
+                            print(f"\nAdded a score of {grade_input} to student {student_name}.\n{line_break}")
+                            while True:
+                                add_more = input(f"Would you like to add more grades to student {name_query}?\nEnter \"Add\" or \"Cancel\".\nInput: ").lower()
+                                if add_more == "add":
+                                    print("\nReturning to grade query.")
+                                    break
+                                elif add_more == "cancel":
+                                    print("\nReturning to Home.")
+                                    return
+                                else:
+                                    print("\nInput not recognized.\n")
+                        else:
+                            print("Grade was over 100 or under 0.")
+                    except:
+                        print("Grade was not a recognizable integer.")
+        if not(name_recognized):
+            print("\nName not recognized.\n")
 
 def view_summary():
-    print("view summary")
+    highest_grade = 0
+    highest_students = []
+    print(line_break+"\nGrade summary:",end="")
+    for student in gradebook:
+        if len(gradebook[student]) != 0:
+            print(f"\n - Grades of student {student}: ",end="")
+            print(*gradebook[student],sep=", ",end="")
+            student_average = 0
+            for grade in gradebook[student]:
+                student_average+=grade/len(gradebook[student])
+            if len(gradebook[student]) != 1:
+                print(f" Average: {student_average:.2f}",end="")
+            if student_average > highest_grade:
+                highest_grade = student_average
+                highest_students = [student]
+            elif student_average == highest_grade:
+                highest_students.append(student)
+        else:
+            print(f"\n - {student} has no grades.",end="")
+    print("\nHighest grade belongs to ",end="")
+    print(*highest_students,sep=", ",end="")
+    print(f" with a grade of {highest_grade:.2f}")
+    
 
 while True:
-    user_input = input("\nWhat would you like to do?\nOptions: \"Add Student\", \"Add Grades\", \"View Summary\", or \"Exit\".\nSelect: ").lower()
+    user_input = input(line_break+"\nWhat would you like to do?\nOptions: \"Add Student\", \"Add Grades\", \"View Summary\", or \"Exit\".\nSelect: ").lower()
     if user_input in ("add student", "add students", "student", "students"):
         add_student()
     elif user_input in ("add grade", "add grades", "grade", "grades"):
@@ -70,4 +141,4 @@ while True:
     elif user_input in ("end", "exit", "stop", "leave"):
         break
     else:
-        print("User input not recognized.")
+        print("\nUser input not recognized.")
